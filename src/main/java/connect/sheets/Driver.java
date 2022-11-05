add8f5b1655466f2172352df74e968b1f379f77b
mtiessler <max.tiessler@estudiantat.upc.edu>
2022-11-05 16:36:25 +0100

    improved GoogleSheets Driver

--- src/main/java/connect/sheets/Driver.java
+++ src/main/java/connect/sheets/Driver.java
@@ -5,18 +5,26 @@ import util.PropertyFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Runs the SheetsSourceTask without connector
+ * @author Max Tiessler
+ */
 public class Driver {
     public static void main(String[] args) throws InterruptedException {
 
+        Logger driverLogger = Logger.getLogger(Driver.class.getName());
         Map<String, String> config = PropertyFile.get("./config/connect-sheets.properties");
         SheetsSourceTask sheetsSourceTask = new SheetsSourceTask();
         sheetsSourceTask.start(config);
-
         while (true) {
             List<SourceRecord> records = sheetsSourceTask.poll();
             for (SourceRecord sr : records) {
                 System.out.println(sr);
+                String sourceRecordResult = sr.toString();
+                driverLogger.log(Level.INFO, "Result: {}", sourceRecordResult);
             }
         }
     }
